b2e477933e14e4f7fdd16d21758312f7811b8ec3
Igor Smugalov <smugalovigor@gmail.com>
2023-11-26 18:45:54 +0700

    feat: services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MessagesComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HeroesComponent],
   providers: [],
   bootstrap: [AppComponent],
