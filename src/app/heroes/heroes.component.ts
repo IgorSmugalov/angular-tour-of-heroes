e0f10ae1c64083a48e182076bdecf41c7ae717d4
Igor Smugalov <smugalovigor@gmail.com>
2023-11-26 16:54:33 +0700

    feat: hero details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -9,7 +10,7 @@ import { HEROES } from '../mock-heroes';
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
-  imports: [FormsModule, NgIf, NgFor, UpperCasePipe],
+  imports: [FormsModule, NgIf, NgFor, UpperCasePipe, HeroDetailComponent],
 })
 export class HeroesComponent {
   heroes = HEROES;
